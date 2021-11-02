2be97192603cc176ba905e77e794297d14551ec0
SeroGri <sero.grigoryan@gmail.com>
2021-11-02 03:54:08 -0700

    loops

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,10 +6,21 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+for (var i = 0; i < enemyNames.length; i++) {
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    // call fight function with enemy-robot
+    fight(pickedEnemyName);
+}
 
 var fight = function(enemyNames) {
-    window.alert("Welcome to Robot Gladiators!"); // fight function statement 
-}
+    // repeat and execute as long as the enemy-robot is alive
+    while(enemyHealth > 0) {
+        // place fight function code block here
+    }; // fight function statement 
+
+
+
   
     //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
   enemyHealth = enemyHealth - playerAttack;
@@ -85,12 +96,9 @@ if (playerHealth <= 0) {
     }
 }
 
-for (var i=0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
-}
+
 // Game States
 // "WIN" - Player robot has defeated all enemy-robots
 //    * Fight all enemy-robots
 //    * Defeat each enemy-robot
 // "LOSE" - Player robot's health is zero or less
-
