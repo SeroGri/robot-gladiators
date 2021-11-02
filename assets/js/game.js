716d81e9fc3c73a9bdede7cbdbc7b1efbd47b293
SeroGri <sero.grigoryan@gmail.com>
2021-11-02 03:27:51 -0700

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,35 +2,33 @@ var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
-
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
-var enemyAttack = 100;
+var enemyAttack = 12;
 
-var fight = function() {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+
+var fight = function(enemyNames) {
+    window.alert("Welcome to Robot Gladiators!"); // fight function statement 
+}
   
     //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
   enemyHealth = enemyHealth - playerAttack;
     // Log a resulting message to the console so we know that it worked.
   console.log(
-      playerName + " attacked " + enemyName + ". " + enemyName, " now has " + enemyHealth + " health remaining." );
+      playerName + " attacked " + enemyNames + ". " + enemyNames, " now has " + enemyHealth + " health remaining." );
 
       // check enemy's health
       if (enemyHealth <= 0) {
-          window.alert(enemyName + " has died! ");
+          window.alert(enemyNames + " has died! ");
       }
       else {
-          window.alert(enemyName + " still has " + enemyHealth + " health left.");
+          window.alert(enemyNames + " still has " + enemyHealth + " health left.");
       }
     // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
   playerHealth = playerHealth - enemyAttack;
     // Log a resulting message to the console so we know that it worked.
   console.log(
-      enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining "
+      enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining "
   );
 
   // check player's health
@@ -40,27 +38,27 @@ var fight = function() {
     else {
         window.alert(playerName + " still has " + playerHealth + " health left.");
     }
-};
+
 
 var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     if (promptFight === "FIGHT" || promptFight === "fight") {
         // remove the enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
     console.log(
-        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+        playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining."
     );
 
     // check player's health
 
 if (playerHealth <= 0) {
-    window.alert(enemyName + " has died!");
+    window.alert(enemyNames + " has died!");
     } else {
-        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+        window.alert(enemyNames + " still has " + enemyHealth + " health left.");
     }
     
     // remove player's health by subtracting the amount set in the enemyAttack variable
     playerHealth = playerHealth - enemyAttack;
-    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    console.log(enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
     
     // check player's health
@@ -83,7 +81,16 @@ if (playerHealth <= 0) {
     } 
     // if no (false), ask questions again by running fight() again
     else {
-        fight();
+        // fight();
     }
 }
+
+for (var i=0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
 
